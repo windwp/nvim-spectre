ebf7bf6c0f20fd344716b2c46f3461d0f2447137
Nurdaulet Kurenshe <nurdaulet.kurenshe@med365.kz>
2024-03-02 22:12:17 +0600

    Add highlighting to result lnum

--- lua/spectre/highlight.lua
+++ lua/spectre/highlight.lua
@@ -5,9 +5,10 @@ M.set_hl = function()
     vim.api.nvim_set_hl(0, 'SpectreBody', { link = "String" })
     vim.api.nvim_set_hl(0, 'SpectreFile', { link = "Keyword" })
     vim.api.nvim_set_hl(0, 'SpectreDir', { link = "Comment" })
+    vim.api.nvim_set_hl(0, 'SpectreLine', { link = "Comment" })
     vim.api.nvim_set_hl(0, 'SpectreSearch', { link = "DiffChange" })
     vim.api.nvim_set_hl(0, 'SpectreBorder', { link = "Comment" })
-    vim.api.nvim_set_hl(0, 'SpectreReplace', { link = "DiffDelete" })
+    vim.api.nvim_set_hl(0, 'SpectreReplace', { link = "DiffAdd" })
 end
 
 vim.api.nvim_create_autocmd('ColorScheme', {
